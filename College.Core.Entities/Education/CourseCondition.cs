80a116732caa4fbe874ae18e19e100a15ccb75fe
Minh Nghê <minh.nghengoc@gmail.com>
2024-08-07 09:36:09 +0700

    add Course Condition

---
+++ College.Core.Entities/Education/CourseCondition.cs
@@ -0,0 +1,13 @@
+﻿using College.Commons;
+
+namespace College.Core.Entities
+{
+    public class CourseCondition : BaseEntity
+    {
+        public long CourseId { get; set; }
+        public virtual Course Course { get; set; }
+        public long? RequiredCourseId { get; set; }
+        public virtual Course RequiredCourse { get; set; }
+        public int RequiredMinNumOfCredit { get; set; }
+    }
+}
--- College.Core.Infrastructure/AppDbContext.cs
+++ College.Core.Infrastructure/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using College.Core.Entities;
+using College.Core.Entities.HumanResource;
+using Microsoft.EntityFrameworkCore;
 
 namespace College.Core.Infrastructure
 {
@@ -7,5 +9,39 @@ namespace College.Core.Infrastructure
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Ward> Ward { get; set; }
+        public DbSet<WorkShift> WorkShift { get; set; }
+        public DbSet<AdministrativeRegion> AdministrativeRegion { get; set; }
+        public DbSet<AdministrativeUnit> AdministrativeUnit { get; set; }
+        public DbSet<Allowance> Allowance { get; set; }
+        public DbSet<Benefit> Benefit { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<District> District { get; set; }
+        public DbSet<JobTitle> JobTitle { get; set; }
+        public DbSet<MeetingRoom> MeetingRoom { get; set; }
+        public DbSet<Position> Position { get; set; }
+        public DbSet<Province> Province { get; set; }
+        public DbSet<RequestType> RequestType { get; set; }
+        public DbSet<Subject> Subject { get; set; }
+        public DbSet<LecturerAssignment> LecturerAssignment { get; set; }
+        public DbSet<LecturerAssignmentSchedule> LecturerAssignmentSchedule { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<CourseClass> CourseClass { get; set; }
+        public DbSet<CourseCondition> CourseCondition { get; set; }
+        public DbSet<CourseDepartment> CourseDepartment { get; set; }
+        public DbSet<CourseSubject> CourseSubject { get; set; }
+        public DbSet<EducationMasterPlan> EducationMasterPlan { get; set; }
+        public DbSet<EducationYear> EducationYear { get; set; }
+        public DbSet<ContractBenefit> ContractBenefit { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<EmployeeSalary> EmployeeSalary { get; set; }
+        public DbSet<Contract> Contract { get; set; }
+        public DbSet<ContractAllowance> ContractAllowance { get; set; }
+        public DbSet<Interview> Interview { get; set; }
+        public DbSet<RecruitmentRequest> RecruitmentRequest { get; set; }
+        public DbSet<RecruitmentRequestDetail> RecruitmentRequestDetail { get; set; }
+        public DbSet<Candidate> Candidate { get; set; }
+        public DbSet<EducationProcess> EducationProcess { get; set; }
     }
 }
